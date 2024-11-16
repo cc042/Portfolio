855996236193adc5b04c28ed80c7a9e58caafb1a
cc042 <youssefkhalifa042@gmail.com>
2024-11-16 03:13:39 +0200

    Add files via upload

--- JS/contact.js
+++ JS/contact.js
@@ -1,12 +1,29 @@
-// Contanct Form
+// Form
 const form = document.querySelector(".whatsapp")
 const TextArea = document.querySelector("textarea")
 const Email = document.querySelector("#EmailAdress")
 const PhoneNumer = document.querySelector("#PhoneNumer")
 const name1 = document.querySelector("#firstName")
 const name2 = document.querySelector("#secondName")
+
+// Send
 const send = document.querySelector(".SubmitForm")
+
+// Clear Form
+const Clear_Form = document.querySelector(".Clear_Form")
+
+// Saved Data
 const savedText = document.querySelector(".savedText")
+
+// Error
+const error = document.querySelector(".Error")
+const ErrorMessage = document.querySelector("#ErrorMessage")
+const ErrorClose = document.querySelector(".closeError")
+
+// Succses
+const Succses = document.querySelector(".Sucsses")
+const SucssesMessage = document.querySelector("#SucssesMessage")
+const SuccsesClose = document.querySelector(".closeSucsses")
 
 function submittion() {
 	var SavedText = `
@@ -14,7 +31,8 @@ function submittion() {
 	%0AEmail: ${Email.value.trim()}
 	%0APhone Number: ${PhoneNumer.value.trim()}`;
 
-	var Saved_Text = SavedText.replaceAll("%0A", "<br>")
+	var Saved_Text = SavedText.replaceAll("%0A", "<br>");
+	localStorage.setItem("Saved_Message", Saved_Text);
 
 	// Send the text to the WhatsApp number
 	let text = `
@@ -27,7 +45,29 @@ function submittion() {
 
 	// Say Sended
 	speechSynthesis.speak(new SpeechSynthesisUtterance("Sended"))
+}if (localStorage.getItem('Saved_Message') == null) {
+	savedText.innerHTML = `Name:<br>Email:<br>Phone Number:`;
+	Clear_Form.style.boxShadow = "0 0 20px var(--bittersweet-shimmer)"
+} else {
+	savedText.innerHTML = localStorage.getItem('Saved_Message');
+	Clear_Form.style.boxShadow = "0 0 20px #FFDB70"
+}
+
+function Clear_Contact() {
+	if (localStorage.getItem('Saved_Message') == null) {
+		error.showModal()
+		ErrorMessage.textContent = "There Is No SomeThing Stored To Remove"
+		ErrorClose.addEventListener("click", () => { error.close() })
+	}
+	else {
+		Succses.showModal()
+		SucssesMessage.textContent = "Saved Data Removed successfully"
+		SuccsesClose.addEventListener("click", () => { Succses.close() })
+		Clear_Form.style.boxShadow = "0 0 20px var(--bittersweet-shimmer)";
+		localStorage.removeItem("Saved_Message");
+		savedText.innerHTML = "Name:<br>Email:<br>Phone Number:";
+	}
 }
 
 form.addEventListener("submit", submittion)
-savedText.innerHTML = localStorage.getItem('Saved_Message')
+Clear_Form.addEventListener("click", Clear_Contact)
--- JS/cookies.js
+++ JS/cookies.js
@@ -15,6 +15,55 @@ const cookieSubmit = document.querySelector(".cookieSubmit");
 localStorage.getItem("name")
 localStorage.getItem("userstate")
 
+// Functions
+function prevent_leakage() {
+    var msg1 = "Sorry, This behavior is disabled to prevent leakage of confidential functions";
+    var msg2 = "Sorry, This Button Is Disabled To Avoid Errors";
+
+    // Sounds
+    var talk1 = new SpeechSynthesisUtterance(msg1);
+    var talk2 = new SpeechSynthesisUtterance(msg2);
+
+    document.addEventListener('contextmenu', function (e) {
+        window.speechSynthesis.speak(talk1)
+        e.preventDefault();
+    });
+
+    document.onkeydown = function (e) {
+        if (event.keyCode == 123) {
+            window.speechSynthesis.speak(talk1);
+            return false;
+        }
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+            window.speechSynthesis.speak(talk1);
+            return false;
+        }
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+            window.speechSynthesis.speak(talk1);
+            return false;
+        }
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+            window.speechSynthesis.speak(talk1);
+            return false;
+        }
+        if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+            window.speechSynthesis.speak(talk1);
+            return false;
+        }
+        if (e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) {
+            window.speechSynthesis.speak(talk1);
+            return false;
+        } if (e.ctrlKey && e.keyCode == 'A'.charCodeAt(0)) {
+            window.speechSynthesis.speak(talk1);
+            return false;
+        }
+        if (e.key == "Escape") {
+            window.speechSynthesis.speak(talk2);
+            return false;
+        }
+    }
+}
+
 setTimeout(() => {
     if (localStorage.getItem("dialog") !== "done") {
         dialogCookies.showModal()
@@ -24,6 +73,10 @@ setTimeout(() => {
         dialogCookies.close()
     }
 }, 1000)
+
+if (localStorage.getItem("userstate") !== "admin") {
+    prevent_leakage()
+}
 
 cookiesform.addEventListener("submit", e => {
     e.preventDefault()
@@ -36,7 +89,6 @@ function submitForm() {
 
     if (Name) {
         if (localStorage.getItem("name") == "adhamati" || localStorage.getItem("name") == "code comet") {
-
             // User State {Admin or Normal}
             userState = `admin`
             localStorage.setItem("userstate", userState)
